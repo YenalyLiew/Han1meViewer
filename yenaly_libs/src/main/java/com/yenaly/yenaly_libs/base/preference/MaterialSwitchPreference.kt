de78eb6e625f3c943cfd6a4bd37b08986691b4ba
Yenaly <wrzg860546@gmail.com>
2024-09-06 00:46:28 +0800

    [优化] MD3视觉效果

---
+++ yenaly_libs/src/main/java/com/yenaly/yenaly_libs/base/preference/MaterialSwitchPreference.kt
@@ -0,0 +1,14 @@
+package com.yenaly.yenaly_libs.base.preference
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.preference.SwitchPreferenceCompat
+import com.yenaly.yenaly_libs.R
+
+open class MaterialSwitchPreference @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null,
+): SwitchPreferenceCompat(context, attrs) {
+    init {
+        widgetLayoutResource = R.layout.yenaly_preference_switch_widget
+    }
+}
