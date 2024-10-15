4f2f67128f1ef85dfb2741445327e5c694c010a2
Yenaly <wrzg860546@gmail.com>
2024-10-15 19:39:45 +0800

    [优化] debug版本号固定

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -25,18 +25,31 @@ object Config {
          * 创建版本号
          *
          * @return 版本号和版本名
+         *
+         *         Example:
+         *         ci: 0.1.2-ci+21032500
+         *         release: 0.1.2-release+21032500
+         *         debug (fixed): debug+1
          */
         fun Project.createVersion(
             major: Int, minor: Int, patch: Int
         ): Pair<Int, String> {
             val source = this.source
-            val versionCode = when (source) {
-                DEBUG -> 1
-                else -> LocalDateTime.now(Clock.systemUTC()).format(
-                    DateTimeFormatter.ofPattern("yyMMddHH")
-                ).toInt()
+            val versionCode: Int
+            val versionName: String
+            when (source) {
+                DEBUG -> {
+                    versionCode = 1
+                    versionName = "$DEBUG+$versionCode"
+                }
+
+                else -> {
+                    versionCode = LocalDateTime.now(Clock.systemUTC()).format(
+                        DateTimeFormatter.ofPattern("yyMMddHH")
+                    ).toInt()
+                    versionName = "${major}.${minor}.${patch}-$source+$versionCode"
+                }
             }
-            val versionName = "${major}.${minor}.${patch}-$source+$versionCode"
             println("Version Code: $versionCode")
             println("Version Name: $versionName")
             return versionCode to versionName
