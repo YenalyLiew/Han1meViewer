305044da1cbd910b00a2ca8ad97fe1e80d470a9c
Yenaly <wrzg860546@gmail.com>
2024-10-18 10:41:27 +0800

    [修复] 播放界面全屏后无法返回正常界面

--- yenaly_libs/src/main/java/com/yenaly/yenaly_libs/ActivityManager.kt
+++ yenaly_libs/src/main/java/com/yenaly/yenaly_libs/ActivityManager.kt
@@ -1,5 +1,6 @@
 package com.yenaly.yenaly_libs
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.util.Log
@@ -19,6 +20,33 @@ object ActivityManager {
     @JvmStatic
     var currentActivity: WeakReference<Activity?> = WeakReference(null)
         internal set
+
+    private val topActivityByReflect: Activity?
+        @SuppressLint("PrivateApi")
+        get() {
+            try {
+                val activityThreadCls = Class.forName("android.app.ActivityThread")
+                val activityThread = activityThreadCls.getMethod("currentActivityThread")(null)
+                val activitiesField = activityThreadCls.getDeclaredField("mActivities")
+                activitiesField.isAccessible = true
+                val activities = activitiesField[activityThread] as Map<*, *>?
+                if (activities == null) return null
+                activities.values.forEach { activityRecord ->
+                    val activityRecordCls = activityRecord?.javaClass
+                    activityRecordCls?.getDeclaredField("paused")?.also { pausedField ->
+                        pausedField.isAccessible = true
+                        if (!pausedField.getBoolean(activityRecord)) {
+                            val activityField = activityRecordCls.getDeclaredField("activity")
+                            activityField.isAccessible = true
+                            return activityField[activityRecord] as Activity
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return null
+        }
 
     @JvmStatic
     fun exit(killProcess: Boolean = true) {
